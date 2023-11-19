1b43f6486b919033da4160843e594fd3dc8f29a8
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-19 15:39:11 -0800

    Implemented the correct way to server.delete, all at once

--- api/server.js
+++ api/server.js
@@ -49,7 +49,24 @@ server.get("/api/users/:id", async (req, res) => {
     })
   }
 })
+server.delete("/api/users/:id", async(req, res) => {
+  try{
+    const { id } = req.params
+    const foundUser = await User.remove(id)
+    if(!foundUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      res.status(202).json(foundUser)
+    }
 
+  } catch {
+    res.status(500).json({
+      message: "The user could not be removed"
+    })
+  }
+})
 
 
 
