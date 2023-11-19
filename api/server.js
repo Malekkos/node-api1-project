0ec3ec560d1e6fc32fa14f274e92cdc101efc7e0
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-19 14:57:17 -0800

    Created try and catch, destructured id name and bio from req.body, created conditonal and a subsequent error response for missing data on either name or bio. On success, creates a createdUser variable using the function insert from User. Then, pass a message as well as a createdUser. Current error with testing, id 'has no path' and expected value from data is wrong

--- api/server.js
+++ api/server.js
@@ -1,18 +1,31 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
 const server = express()
+const User = require("./users/model")
 
 server.use(express.json())
 
 server.post("/api/users", async (req, res) => {
-  console.log("this is the request body: ", req.body, "this is the response params: ", res.params)
+  // console.log("this is the request body: ", req.body, "this is the response params: ", res.params)
   // res.status(200).json({ message: "hello, world"})
   try {
-    const { id, name, bio } = req.body
+    const { id, name, bio } = req.body // Does need an id?
+    if(!name || !bio) {
+      res.status(400).json({ //needs to be 400, not 422
+        message: "Please provide name and bio for the user"
+      })  
+    } else {
+    const createdUser = await User.insert({ name, bio }) //expects only name and bio, creates its own ID
     // console.log(id, name, bio) // works
-  } catch (err) {
+    // console.log(createdUser)
+    res.status(201).json({ //created
+      message: "success creating a new user",
+      data: createdUser,
+    })
+  }
+  } catch {
     res.status(500).json({
-      message: `Error creating dog:${err.message}`
+      message: "There was an error while saving the user to the database"
     })
   }
 })
