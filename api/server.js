7f0bcfa2120a73ba840c64011cd55ce9d7e6691a
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-19 15:34:02 -0800

    Created server.get for id

--- api/server.js
+++ api/server.js
@@ -30,10 +30,27 @@ server.get("/api/users", async (req, res) => {
     res.status(500).json({
       message: "The users information could not be retrieved"
     })
-
   }
 })
-
+server.get("/api/users/:id", async (req, res) => {
+  console.log("req.params", req.params, "res.body", res.body)
+  try {
+    const { id } = req.params
+    const foundUser = await User.findById(id)
+    if(!foundUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+    console.log(id)
+    res.status(200).json(foundUser)
+  }
+  } catch {
+    res.status(500).json({
+      message: "The user information could not be retrieved"
+    })
+  }
+})
 
 
 
