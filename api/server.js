2789493f6a10f6f7b870073fe3d22b648edc1426
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-19 14:16:36 -0800

    Setup a preliminary post method, loggin req.body lets me see, with postman, that it is in fact getting a body

--- api/server.js
+++ api/server.js
@@ -1,8 +1,17 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
-
 const server = express()
 
 server.use(express.json())
+
+server.post("/api/users", (req, res) => {
+  console.log("this is the request body: ", req.body, "this is the response: ", res.params)
+  res.status(200).json({ message: "hello, world"})
+})
+
+
+
+
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}///
