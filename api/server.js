bd038a968ef6903ae3c1b29d55c35fad0d25238f
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-19 15:02:35 -0800

    Cleaned up comments and console logs

--- api/server.js
+++ api/server.js
@@ -6,18 +6,14 @@ const User = require("./users/model")
 server.use(express.json())
 
 server.post("/api/users", async (req, res) => {
-  // console.log("this is the request body: ", req.body, "this is the response params: ", res.params)
-  // res.status(200).json({ message: "hello, world"})
   try {
-    const { id, name, bio } = req.body // Does need an id?
+    const { name, bio } = req.body
     if(!name || !bio) {
-      res.status(400).json({ //needs to be 400, not 422
+      res.status(400).json({
         message: "Please provide name and bio for the user"
       })  
     } else {
-    const createdUser = await User.insert({ name, bio }) //expects only name and bio, creates its own ID
-    // console.log(id, name, bio) // works
-    console.log(createdUser)
+    const createdUser = await User.insert({ name, bio })
     res.status(201).json(createdUser)
   }
   } catch {
